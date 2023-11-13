7044022eb44b7cd177c3fcc48a9373b4b10e2513
w6rsty <1486008923@qq.com>
2023-11-13 18:43:20 +0800

    save progress

--- include/container/sparse_set.hpp
+++ include/container/sparse_set.hpp
@@ -1,14 +1,20 @@
 #pragma once
+#include <array>
+#include <limits>
 #include <type_traits>
+#include <memory>
 #include <vector>
-
 
 namespace pecs {
 
 template <typename T, size_t PageSize, typename = std::enable_if<std::is_integral_v<T>>>
 class SparseSet final {
 private:
-
+    std::vector<T> _packed;
+    std::unique_ptr<std::array<T, PageSize>> _sparse;
+    static constexpr T null = std::numeric_limits<T>::max();
+    
+     
 public:
     void add() {
 
@@ -23,7 +29,7 @@ public:
     }
 
     void clear() {
-        
+
     }
 };
 
